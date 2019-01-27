66a234eb9043bb5a36bb3e9a38dcee1260289c1f
Rupayan Neogy <rneogy@mit.edu>
2019-01-27 15:31:00 -0500

    lobbies!

--- Assets/Scripts/CorePlayerController.cs
+++ Assets/Scripts/CorePlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class PlayerController : NetworkBehaviour
+public class CorePlayerController : NetworkBehaviour
 {
     FirefighterController fc;
     ArsonistController ac;
